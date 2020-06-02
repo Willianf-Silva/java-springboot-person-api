ebe9961af509280a8933496051ba8243d332172f
Willianf-Silva <willian.ferreira.da.silva@gmail.com>
2020-06-02 20:40:07 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/javaspringbootpersonapi/JavaSpringbootPersonApiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.javaspringbootpersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootPersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootPersonApiApplication.class, args);
+	}
+
+}
