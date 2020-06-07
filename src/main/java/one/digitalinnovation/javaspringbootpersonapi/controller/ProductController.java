a4dcbec29e23cc2195b9e819b4742dc7d4a10ab3
Willianf-Silva <willian.ferreira.da.silva@gmail.com>
2020-06-07 20:35:09 -0300

    Developed GET product findById and Developed GET product listAll

--- src/main/java/one/digitalinnovation/javaspringbootpersonapi/controller/ProductController.java
+++ src/main/java/one/digitalinnovation/javaspringbootpersonapi/controller/ProductController.java
@@ -2,12 +2,13 @@ package one.digitalinnovation.javaspringbootpersonapi.controller;
 
 import one.digitalinnovation.javaspringbootpersonapi.dto.MessageResponseDTO;
 import one.digitalinnovation.javaspringbootpersonapi.dto.request.ProductDTO;
+import one.digitalinnovation.javaspringbootpersonapi.entity.Product;
+import one.digitalinnovation.javaspringbootpersonapi.exception.RecursoNotFoundException;
 import one.digitalinnovation.javaspringbootpersonapi.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/product")
@@ -22,5 +23,15 @@ public class ProductController {
     @PostMapping
     public MessageResponseDTO createProduct(@RequestBody ProductDTO productDTO){
         return productService.createProduct(productDTO);
+    }
+
+    @GetMapping
+    public List<Product> listAll(){
+        return productService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDTO findById(@PathVariable Long id) throws RecursoNotFoundException {
+        return productService.findById(id);
     }
 }
--- src/main/java/one/digitalinnovation/javaspringbootpersonapi/service/ProductService.java
+++ src/main/java/one/digitalinnovation/javaspringbootpersonapi/service/ProductService.java
@@ -2,11 +2,16 @@ package one.digitalinnovation.javaspringbootpersonapi.service;
 
 import one.digitalinnovation.javaspringbootpersonapi.dto.MessageResponseDTO;
 import one.digitalinnovation.javaspringbootpersonapi.dto.request.ProductDTO;
+import one.digitalinnovation.javaspringbootpersonapi.entity.Person;
 import one.digitalinnovation.javaspringbootpersonapi.entity.Product;
+import one.digitalinnovation.javaspringbootpersonapi.exception.RecursoNotFoundException;
 import one.digitalinnovation.javaspringbootpersonapi.mapper.ProductMapper;
 import one.digitalinnovation.javaspringbootpersonapi.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -24,9 +29,23 @@ public class ProductService {
         return messageResponse("Created product with id ", savedProduct.getId());
     }
 
+
+    public List<Product> listAll() {
+        return productRepository.findAll();
+    }
+
+    public ProductDTO findById(Long id) throws RecursoNotFoundException {
+        verifyIfExists(id);
+        return productMapper.toDTO(productRepository.findById(id).get());
+    }
+
     private MessageResponseDTO messageResponse(String message, Long id) {
         return MessageResponseDTO.builder()
                 .message(message + id)
                 .build();
+    }
+
+    private Product verifyIfExists(Long id) throws RecursoNotFoundException {
+        return productRepository.findById(id).orElseThrow(()-> new RecursoNotFoundException("Person not found with ID ", id));
     }
 }
