2f4bf34bd8051b107675e6dd56443252e2cf7a82
Willianf-Silva <willian.ferreira.da.silva@gmail.com>
2020-06-17 17:54:31 -0300

    included mapping productDTO to product and product to productDTO

--- src/main/java/one/digitalinnovation/javaspringbootpersonapi/mapper/ProductItemMapper.java
+++ src/main/java/one/digitalinnovation/javaspringbootpersonapi/mapper/ProductItemMapper.java
@@ -3,13 +3,16 @@ package one.digitalinnovation.javaspringbootpersonapi.mapper;
 import one.digitalinnovation.javaspringbootpersonapi.dto.request.ProductItemDTO;
 import one.digitalinnovation.javaspringbootpersonapi.entity.ProductItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ProductItemMapper {
     ProductItemMapper INSTANCE = Mappers.getMapper(ProductItemMapper.class);
 
+    @Mapping(target = "product", source = "productDTO")
     ProductItem toModel(ProductItemDTO productItemDTO);
 
+    @Mapping(target = "productDTO", source = "product")
     ProductItemDTO toDTO(ProductItem productItem);
 }
