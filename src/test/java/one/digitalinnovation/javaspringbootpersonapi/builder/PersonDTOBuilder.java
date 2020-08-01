7f6dd21b3ddf4295548c4d8e573950daaa51cb2d
Willianf-Silva <willian.ferreira.da.silva@gmail.com>
2020-08-01 15:24:44 -0300

    Added phone build and person build for test

--- src/test/java/one/digitalinnovation/javaspringbootpersonapi/builder/PersonDTOBuilder.java
+++ src/test/java/one/digitalinnovation/javaspringbootpersonapi/builder/PersonDTOBuilder.java
@@ -1,4 +1,34 @@
 package one.digitalinnovation.javaspringbootpersonapi.builder;
 
+import lombok.Builder;
+import one.digitalinnovation.javaspringbootpersonapi.dto.request.PersonDTO;
+import one.digitalinnovation.javaspringbootpersonapi.dto.request.PhoneDTO;
+
+import java.util.Collections;
+import java.util.List;
+
+@Builder
 public class PersonDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String firstName = "Bruno";
+
+    @Builder.Default
+    private String lastName = "Silva";
+
+    @Builder.Default
+    private String cpf = "369.333.878-79";
+
+    @Builder.Default
+    private String birthDate = "23-03-1988";
+
+    @Builder.Default
+    private List<PhoneDTO> phones = Collections.singletonList(PhoneDTOBuilder.builder().build().toPhoneDTO());
+
+    public PersonDTO toPersonDTO(){
+        return new PersonDTO(id,firstName,lastName,cpf,birthDate,phones);
+    }
 }
--- src/test/java/one/digitalinnovation/javaspringbootpersonapi/builder/PhoneDTOBuilder.java
+++ src/test/java/one/digitalinnovation/javaspringbootpersonapi/builder/PhoneDTOBuilder.java
@@ -1,4 +1,22 @@
 package one.digitalinnovation.javaspringbootpersonapi.builder;
 
+import lombok.Builder;
+import one.digitalinnovation.javaspringbootpersonapi.dto.request.PhoneDTO;
+import one.digitalinnovation.javaspringbootpersonapi.enums.PhoneType;
+
+@Builder
 public class PhoneDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private PhoneType type = PhoneType.MOBILE;
+
+    @Builder.Default
+    private String number = "(19)999999999";
+
+    public PhoneDTO toPhoneDTO(){
+        return new PhoneDTO(id,type,number);
+    }
 }
