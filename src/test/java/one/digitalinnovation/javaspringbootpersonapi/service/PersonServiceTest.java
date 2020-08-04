cf33fe6d95829ae4e862e6a8428b54aa1406b54d
Willianf-Silva <willian.ferreira.da.silva@gmail.com>
2020-08-04 20:07:55 -0300

    Developed test for find by id and delete persons

--- src/test/java/one/digitalinnovation/javaspringbootpersonapi/service/PersonServiceTest.java
+++ src/test/java/one/digitalinnovation/javaspringbootpersonapi/service/PersonServiceTest.java
@@ -19,10 +19,12 @@ import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
+    private static final Long INVALID_PERSON_ID = 2L;
 
     @Mock
     PersonRepository personRepository;
@@ -79,4 +81,57 @@ public class PersonServiceTest {
 
         assertThat(foundListPersonsDTO, is(empty()));
     }
+
+    @Test
+    void whenValidPersonIdIsGivenThenReturnAProduct() throws RecursoNotFoundException {
+        // given
+        PersonDTO personDTO = PersonDTOBuilder.builder().build().toPersonDTO();
+        Person expectedPerson = personMapper.toModel(personDTO);
+
+        // when
+        when(personRepository.findById(personDTO.getId())).thenReturn(Optional.of(expectedPerson));
+
+        //then
+        PersonDTO foundPersonDTO = personService.findById(personDTO.getId());
+        assertThat(foundPersonDTO.getCpf(), is(equalTo(personDTO.getCpf())));
+    }
+
+    @Test
+    void whenNotRegisteredPersonIdIsGivenThenThrowAnException() {
+        // given
+        PersonDTO expectedFoundPersonDTO = PersonDTOBuilder.builder().build().toPersonDTO();
+
+        // when
+        when(personRepository.findById(expectedFoundPersonDTO.getId())).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(RecursoNotFoundException.class, () -> personService.findById(expectedFoundPersonDTO.getId()));
+    }
+
+    @Test
+    void whenExclusionIsCalledWithValidIdThenAPersonShouldBeDeleted() throws RecursoNotFoundException {
+        // given
+        PersonDTO personDTO = PersonDTOBuilder.builder().build().toPersonDTO();
+        Person expectedPersonDeleted = personMapper.toModel(personDTO);
+
+        // when
+        when(personRepository.findById(personDTO.getId())).thenReturn(Optional.of(expectedPersonDeleted));
+        doNothing().when(personRepository).deleteById(personDTO.getId());
+
+        // then
+        personService.delete(personDTO.getId());
+        verify(personRepository, times(1)).findById(personDTO.getId());
+        verify(personRepository, times(1)).deleteById(personDTO.getId());
+    }
+
+    @Test
+    void whenExclusionIsCalledWithInvalidIdThenExceptionShouldBeThrown() {
+        // when
+        when(personRepository.findById(INVALID_PERSON_ID)).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(RecursoNotFoundException.class, ()-> personService.delete(INVALID_PERSON_ID));
+    }
+
+    // continuar implementando upddateById
 }
